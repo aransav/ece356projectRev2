a5afaa477b6bc98fe4ab28c752d6a05b805b5643
sivajega <sivajega@github.com>
2014-03-30 06:09:01 -0400

    new shit

---
+++ proj/src/main/java/ca/uw/proj/service/PatientService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.uw.proj.service;
+
+import ca.uw.proj.model.Patient;
+import ca.uw.proj.model.User;
+
+/**
+ *
+ * @author siva
+ */
+public interface PatientService {
+    public Patient getPatient(User u);
+    public Patient updatePatient(Patient patient);
+}
